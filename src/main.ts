676d7a01d28ff37f08233eb48ab02741f4f4c8ed
Jonathan Noguerales <jonathan.n@pyv.es>
2024-06-05 17:29:36 +0200

    cambios filtro

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 interface Personaje {
   id: number;
   nombre: string;
-  amigos: string;
+  amigo: string;
   apodo: string;
   especialidad: string;
   habilidades: string[];
@@ -34,10 +34,13 @@ const obtenerPersonajes = async (): Promise<Personaje[]> => {
 const crearCardPersonaje = (personaje: Personaje): HTMLDivElement => {
   const card = document.createElement("div");
   card.classList.add("cardPersonajes");
+  const divImagen = document.createElement("div");
+  divImagen.classList.add("divImagen");
+  const divParrafos = document.createElement("div");
 
   const imagen = crearElementoImagen(personaje.imagen, personaje.nombre);
   const nombre = crearElementoParrafo(`Nombre: ${personaje.nombre}`);
-  const amigos = crearElementoParrafo(`Amigos: ${personaje.amigos}`);
+  const amigo = crearElementoParrafo(`Amigos: ${personaje.amigo}`);
   const apodo = crearElementoParrafo(`Apodo: ${personaje.apodo}`);
   const especialidad = crearElementoParrafo(
     `Especialidad: ${personaje.especialidad}`
@@ -46,26 +49,43 @@ const crearCardPersonaje = (personaje: Personaje): HTMLDivElement => {
     `Habilidades: ${personaje.habilidades.join(", ")}`
   );
 
-  card.appendChild(imagen);
-  card.appendChild(nombre);
-  card.appendChild(amigos);
-  card.appendChild(apodo);
-  card.appendChild(especialidad);
-  card.appendChild(habilidades);
+  card.appendChild(divImagen);
+  card.appendChild(divParrafos);
+  divImagen.appendChild(imagen);
+  divParrafos.appendChild(nombre);
+  divParrafos.appendChild(amigo);
+  divParrafos.appendChild(apodo);
+  divParrafos.appendChild(especialidad);
+  divParrafos.appendChild(habilidades);
 
   return card;
 };
 
-const pintarPersonajes = async () => {
+const pintarPersonajes = async (filtro: string = "") => {
   const personajes = await obtenerPersonajes();
   const contenedorCards = document.getElementById("contenedorCards");
 
   if (contenedorCards) {
-    personajes.forEach((personaje) => {
-      const card = crearCardPersonaje(personaje);
-      contenedorCards.appendChild(card);
-    });
+    contenedorCards.innerHTML = "";
+    personajes
+      .filter((personaje) =>
+        personaje.nombre.toLowerCase().includes(filtro.toLowerCase())
+      )
+      .forEach((personaje) => {
+        const card = crearCardPersonaje(personaje);
+        contenedorCards.appendChild(card);
+      });
   }
 };
 
-document.addEventListener("DOMContentLoaded", pintarPersonajes);
+document.addEventListener("DOMContentLoaded", () => {
+  const inputFiltro = document.getElementById("nombre") as HTMLInputElement;
+
+  if (inputFiltro) {
+    inputFiltro.addEventListener("input", () => {
+      pintarPersonajes(inputFiltro.value);
+    });
+  }
+
+  pintarPersonajes();
+});
